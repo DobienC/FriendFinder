02fab02943377f36e6869a15295669de640fb582
DobienC <dobien_c@yahoo.com>
2019-03-27 15:49:59 -0700

    html progress

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 var express = require("express");
 var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+var routes = require("");
