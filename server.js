72fef90482d1feb2e554cf858eaca6ad8dea06ee
DobienC <dobien_c@yahoo.com>
2019-03-27 22:57:58 -0700

    Small html changes

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
-
-app.use(express.static("public"));
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("./app/public"));
 
-var routes = require("");
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
